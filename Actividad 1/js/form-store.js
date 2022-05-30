b1ec3bf88754287d96f92818220037688eae3b65
DavidZS1984 <104953185+DavidZS1984@users.noreply.github.com>
2022-05-29 23:56:15 -0500

    main

---
+++ Actividad 1/js/form-store.js
@@ -0,0 +1,7 @@
+function registerproduct(){
+
+const procduct_name = document.getElementById("").value
+const procduct_cart = document.getElementById("").value
+const procduct_stock = document.getElementById("").value
+
+}
